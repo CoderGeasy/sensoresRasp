a52a5a39e8952a5ecbe61307cdff788e406ad7bb
CoderGeasy <21170116@uttcampus.edu.mx>
2023-03-24 12:25:28 -0600

    temp

--- Temperatura.py
+++ Temperatura.py
@@ -1,14 +1,20 @@
 import sys
 import adafruit_dht
 
+pin=4
+
+sensor = adafruit_dht.DHT11(pin)
 class Temperatura:
     def __init__(self, pin):
         self.sensor = adafruit_dht.DHT11
         self.pin = pin
 
     def medirTemperatura(self):
-        hum, temp = adafruit_dht.read(self.sensor, self.pin)
-        return  hum, temp
+        hum = sensor.humidity
+        temp = sensor.temperature
+
+        # Imprime en la consola las variables temperatura y humedad con un decimal
+        print('Temperatura={0:0.1f} C  Humedad={1:0.1f}%'.format(temp, hum))
         
 
 
